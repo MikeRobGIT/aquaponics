1e691122f0bcbbe7d26c9a8724ca27b3e74372b3
Mike Robateau <michaelrobateau@yahoo.com>
2022-04-11 23:02:52 -0400

    clean up

--- getsensordata.py
+++ getsensordata.py
@@ -11,10 +11,10 @@ import json
 import paho.mqtt.publish as publish
 
 from AtlasI2C import (
-	 AtlasI2C
+    AtlasI2C
 )
 mqtt_hostname = "homeassistant"
-mqtt_auth = { 'username': 'mqtt_user', 'password': '9EV8yw7qsPG%' }
+mqtt_auth = {'username': 'mqtt_user', 'password': '9EV8yw7qsPG%'}
 mqtt_topic = "homeassistant/sensor/aquarium/state"
 caltempC = 0
 minimumDelay = 1.5
@@ -29,20 +29,20 @@ class AquariumSensor:
         hasTemp = self.temp != None and self.temp > 0 and self.temp < 120
         hasPpm = self.ppm != None and self.ppm > 0 and self.ppm < 20000
         hasPh = self.ph != None and self.ph > 0 and self.ph < 8.5
-        self.json = { "temp": self.temp, "ph": self.ph, "ppm": self.ppm }
+        self.json = {"temp": self.temp, "ph": self.ph, "ppm": self.ppm}
         self.clean = hasTemp and hasPpm and hasPh
 
     def tof(self, value):
         return value * 1.8 + 32
 
     def resTemp(self, device):
-        global caltempC    # Needed to modify global var
+        global caltempC  # `Needed to modify global var
         print("-------------------Temp--------------------------")
         device.set_i2c_address(102)  # RTP
         device.query("S,c")
-        caltempC = device.query("R").replace("\x00","").strip()
+        caltempC = device.query("R").replace("\x00", "").strip()
         caltempF = self.tof(float(caltempC))
-        caltempF = round(caltempF,1)
+        caltempF = round(caltempF, 1)
         print("Res Temp: "+str(caltempF))
         if caltempF != None and caltempF > 0:
             try:
@@ -57,10 +57,11 @@ class AquariumSensor:
         print("Compensate pH Temp: "+str(caltempC))
         ph = 0
         if caltempC != 0:
-            ph = float(device.query("RT,"+str(caltempC)).replace("\x00","").strip())
+            ph = float(device.query("RT,"+str(caltempC)
+                                    ).replace("\x00", "").strip())
         else:
-            ph = float(device.query("R").replace("\x00","").strip())
-        ph = round(ph,2)
+            ph = float(device.query("R").replace("\x00", "").strip())
+        ph = round(ph, 2)
         print("Res pH: "+str(ph))
 
         if isinstance(ph, str) is True and "Error" in ph:
@@ -77,7 +78,7 @@ class AquariumSensor:
     def resPPM(self, device):
         print("-------------------PPM---------------------------")
         device.set_i2c_address(100)  # EC
-        ppm = float(device.query("R").replace("\x00","").strip())
+        ppm = float(device.query("R").replace("\x00", "").strip())
         print("Res PPM: "+str(ppm))
         ppm = round(ppm)
         if ppm != None and ppm > 0:
@@ -87,15 +88,18 @@ class AquariumSensor:
                 print(e)
                 print("*** ERROR *** Res PPM Publish Failed")
 
+
 def resInfo(device):
     res = AquariumSensor(device)
 
     if res.clean:
         packageStr = json.dumps(res.json)
         print(packageStr)
-        publish.single(mqtt_topic, packageStr, hostname=mqtt_hostname, auth=mqtt_auth)
+        publish.single(mqtt_topic, packageStr,
+                       hostname=mqtt_hostname, auth=mqtt_auth)
 
     print("-------------------------------------------------")
+
 
 def configure_ha_auto_discovery():
     # config HA auto discovery
@@ -110,15 +114,16 @@ def configure_ha_auto_discovery():
         "unique_id": "aquarium_ppm",
         "device": {
             "identifiers": [
-                "aquarium_monitor_pi_3"
+                "aquarium_monitor"
             ],
             "name": "Aquarium Monitor",
             "model": "Aquarium Monitor",
             "manufacturer": "CRISIS"
         }
     })
-    #print(packageStr)
-    publish.single("homeassistant/sensor/aquariumPPM/config",packageStr,hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
+    # print(packageStr)
+    publish.single("homeassistant/sensor/aquariumPPM/config",
+                   packageStr, hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
 
     sleep(1)
     packageStr = json.dumps({
@@ -131,15 +136,16 @@ def configure_ha_auto_discovery():
         "unique_id": "aquarium_ph",
         "device": {
             "identifiers": [
-                "aquarium_monitor_pi_3"
+                "aquarium_monitor"
             ],
             "name": "Aquarium Monitor",
             "model": "Aquarium Monitor",
             "manufacturer": "CRISIS"
         }
     })
-    #print(packageStr)
-    publish.single("homeassistant/sensor/aquariumPh/config",packageStr,hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
+    # print(packageStr)
+    publish.single("homeassistant/sensor/aquariumPh/config", packageStr,
+                   hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
 
     sleep(1)
     packageStr = json.dumps({
@@ -153,15 +159,16 @@ def configure_ha_auto_discovery():
         "unique_id": "aquarium_temperature",
         "device": {
             "identifiers": [
-                "aquarium_monitor_pi_3"
+                "aquarium_monitor"
             ],
             "name": "Aquarium Monitor",
             "model": "Aquarium Monitor",
             "manufacturer": "CRISIS"
         }
     })
-    #print(packageStr)
-    publish.single("homeassistant/sensor/aquariumT/config",packageStr,hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
+    # print(packageStr)
+    publish.single("homeassistant/sensor/aquariumT/config", packageStr,
+                   hostname=mqtt_hostname, auth=mqtt_auth, retain=True)
 
 
 def main():
@@ -178,6 +185,7 @@ def main():
             print("*** ERROR *** Res Update Failed")
             pass
         time.sleep(delaytime - minimumDelay)
+
 
 if __name__ == '__main__':
     try:
